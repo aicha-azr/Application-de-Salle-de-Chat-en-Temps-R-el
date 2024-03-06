8803d19e0dff946483e11525b822195f8d85e840
aicha-azr <aichaazeroual03@gmail.com>
2024-03-06 12:05:39 +0100

    webSocket connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,21 +6,21 @@ import { Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import { io } from "socket.io-client"; 
 function App() {
-  const [socket, setSocket] = useState(undefined)
-  useEffect(()=>{
-      const socket = io('http://localhost:4041');
-      alert('you are at app page')
-  },[])
+  
   return (
     <>
-    <h1>hello</h1>
-     {/* <Router>
-      <div className='App'>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-        </Routes>
-      </div>
-  </Router>*/}
+     <Router>
+      {/* Route de redirection vers la page d'accueil */}
+      <Route exact path="/">
+        <Redirect to="/home" />
+      </Route>
+
+      {/* Route de la page d'accueil */}
+      <Route path="/home" component={Home} />
+      {/* Ajoutez d'autres routes ici, si nécessaire */}
+
+    </Router>
+   
     </>
   )
 }
